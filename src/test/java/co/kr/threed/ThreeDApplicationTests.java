e3a139fe9f451371b56dc7f3e9e7dca310384271
jihoon <sjh971101@naver.com>
2022-11-08 21:19:43 +0900

    Initial commit

---
+++ src/test/java/co/kr/threed/ThreeDApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.threed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
